0b092fd0df4d5ceba60ee764c95b469ab4fb3975
Bayu Maulana <bayufransdo@gmail.com>
2023-12-27 19:57:26 +0700

    Setup react router 6 and apply componen-based architecture in pages folder

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,3 +1,26 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter } from 'react-router-dom';
+import { Home, EditBook, CreateBook, DeleteBook, ShowBook } from './pages';
 
-export default const route = createBrowserRouter([{ path: '/', element:  }]);
+const route = createBrowserRouter([
+  {
+    path: '/',
+    element: <Home />,
+  },
+  {
+    path: '/books/create',
+    element: <CreateBook />,
+  },
+  {
+    path: '/books/details/:id',
+    element: <ShowBook />,
+  },
+  {
+    path: '/books/edit/:id',
+    element: <EditBook />,
+  },
+  {
+    path: '/books/delete/:id',
+    element: <DeleteBook />,
+  },
+]);
+export default route;
---
+++ frontend/src/pages/index.js
@@ -0,0 +1,5 @@
+export { default as Home } from './Home';
+export { default as ShowBook } from './ShowBook';
+export { default as CreateBook } from './CreateBook';
+export { default as EditBook } from './EditBook';
+export { default as DeleteBook } from './DeleteBook';
